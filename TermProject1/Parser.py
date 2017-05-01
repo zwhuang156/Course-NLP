4b8e4b00c209ea5900b325c47bafa95b2765c49c
saqwedcxz0329 <saqwedcxz0329@gmail.com>
2017-05-01 23:44:40 +0800

    crack baseline

--- TermProject1/Parser.py
+++ TermProject1/Parser.py
@@ -1,30 +1,87 @@
+import pandas as pd
+import csv
+
+
 class Parser():
     """docstring for Parser"""
     def __init__(self):
         self.data_folder = "./data/"
-        self.filter_punc = ",，.．。!！?？-()「」『 』:、; "
+        self.filter_punc = ",，.．。!！?？-()（）「」『 』:：、; "
 
-    def aspect_review(self):
-        index_size = 4
+    '''
+    if parse aspect_term.txt the size = 4
+    else parse test.review.txt the size = 2
+    '''
+    def parse_review(self, file_name, size):
         index = 0
         aspect_review_list = []
-        with open(self.data_folder + 'aspect_review_half.txt') as file:
-            each_aspect = []
+        with open(self.data_folder + file_name) as file:
+            aspect = []
             for line in file:
                 # print line.strip()
                 line = line.strip()
                 for punc in self.filter_punc:
-                    line = line.replace(punc, '')
-                each_aspect.append(line)
+                    line = line.replace(punc, ',')
+                aspect.append(line)
                 index += 1
-                if index == 4:
+                if index == size:
                     index = 0
-                    aspect_review_list.append(each_aspect)
-                    each_aspect = []
+                    aspect_review_list.append(aspect)
+                    aspect = []
         return aspect_review_list
+
+    def test_review(self):
+        index = 0
+        review_id = 0
+        aspect_review_list = {}
+        with open(self.data_folder + 'test_review.txt') as file:
+            for line in file:
+                line = line.strip()
+                if index == 0:
+                    review_id = line
+                    index +=1
+                elif index == 1:
+                    for punc in self.filter_punc:
+                        line = line.replace(punc, ',')
+                    aspect_review_list[review_id] = line
+                    index = 0
+        return aspect_review_list
+
+    def read_test(self):
+        csv_reader = list(csv.reader(open(self.data_folder + 'test.csv')))
+        return csv_reader[1:]
+
+    def read_aspect_term(self):
+        with open(self.data_folder + 'aspect_term.txt') as file:
+            aspect_term = {}
+            for line in file:
+                line = line.strip('\n').split('\t')
+                aspect = line[0]
+                terms = line[1].split(' ')
+                terms.append(aspect)
+                aspect_term[aspect] = terms
+        return aspect_term
+
+
+
+    def negative(self):
+        with open(self.data_folder + 'NTUSD_neg.txt') as file:
+            negative_word = []
+            for line in file:
+                line = line.strip('\n')
+                negative_word.append(line)
+        return negative_word
+
+    def positive(self):
+        with open(self.data_folder + 'NTUSD_pos.txt') as file:
+            negative_word = []
+            for line in file:
+                line = line.strip('\n')
+                negative_word.append(line)
+        return negative_word
+
 
 if __name__ == '__main__':
     parser = Parser()
-    aspect_review_list = parser.aspect_review()
-    for line in aspect_review_list:
-        print (line[1])
+    parser.parse_review('aspect_review_half.txt', 4)
+    print (parser.read_aspect_term())
--- TermProject1/ReviewOpinion.py
+++ TermProject1/ReviewOpinion.py
@@ -4,116 +4,60 @@ from nltk.collocations import *
 from gensim.models import word2vec
 import jieba
 import numpy as np
-from keras.preprocessing import sequence
-from keras.models import Sequential
-from keras.layers import Dense, Embedding
-from keras.layers import LSTM
-from keras.layers import TimeDistributed
 
 
+class ReviewOpinion(object):
+    """docstring for ReviewOpinion"""
+    def __init__(self):
+        self.parser = Parser()
+        # jieba custom setting.
+        jieba.set_dictionary('jieba_dict/dict.txt.big')
 
-batch_size = 1
+        # load stopwords set
+        self.stopwordset = set()
+        with open('jieba_dict/stopwords.txt','r',encoding='utf-8') as sw:
+            for line in sw:
+                self.stopwordset.add(line.strip('\n'))
 
-parser = Parser()
-# jieba custom setting.
-jieba.set_dictionary('jieba_dict/dict.txt.big')
+    def segment(self, line):
+        result = []
+        words = jieba.cut(line, cut_all=False)
+        for word in words:
+            if word not in self.stopwordset:
+                result.append(word)
+        return result
 
-# load stopwords set
-stopwordset = set()
-with open('jieba_dict/stopwords.txt','r',encoding='utf-8') as sw:
-    for line in sw:
-        stopwordset.add(line.strip('\n'))
+    def main(self):
+        file = open('submit.csv', 'w')
+        file.write('Id,Label\n')
+        test_review = self.parser.test_review()
+        test_csv = self.parser.read_test()
+        neg_words = self.parser.negative()
+        pos_words = self.parser.positive()
+        aspect_term = self.parser.read_aspect_term()
+        for line in test_csv:
+            review_id = line[1]
+            target_aspect = line[2]
+            content = test_review[review_id].split(',')
 
-def transform_y(pos_opinion, neg_opinion):
-    y = [0.5]*5
-    for category in pos_opinion:
-        if category == "服務":
-            y[0] = 1.0
-        if category == "環境":
-            y[1] = 1.0
-        if category == "價格":
-            y[2] = 1.0
-        if category == "交通":
-            y[3] = 1.0
-        if category == "餐廳":
-            y[4] = 1.0
-
-    for category in neg_opinion:
-        if category == "服務":
-            y[0] = 0.0
-        if category == "環境":
-            y[1] = 0.0
-        if category == "價格":
-            y[2] = 0.0
-        if category == "交通":
-            y[3] = 0.0
-        if category == "餐廳":
-            y[4] = 0.0
-    return y
-
-def segment(line):
-    result = []
-    words = jieba.cut(line, cut_all=False)
-    for word in words:
-        if word not in stopwordset:
-            result.append(word)
-    return result
-
-
-def review_to_vec(w2v_model):
-    x_train = []
-    y_train = []
-    for line in parser.aspect_review():
-        review = segment(line[1])
-        pos_opinion = []
-        neg_opinion = []
-        if len(line[2]) != 0:
-            pos_opinion = line[2].split('\t')
-        if len(line[3]) != 0:
-            neg_opinion = line[3].split('\t')
-        y_train.append(transform_y(pos_opinion, neg_opinion))
-        sequnence = []
-        for character in review:
-            try:
-                sequnence.append(w2v_model.wv[character])
-            except KeyError:
-                print ("%s not in vocabulary" %character)
-                sequnence.append([0.0] * w2v_model.vector_size)
-        x_train.append(sequnence)
-    return np.asarray(x_train), np.asarray(y_train)
-
-def build_model(timesteps, vector_size):
-    print('Build model...')
-    model = Sequential()
-    model.add(LSTM(128, dropout=0.3, recurrent_dropout=0.2, return_sequences=False, input_shape=(timesteps, vector_size)))
-    model.add(TimeDistributed(Dense(8), input_shape=(timesteps, vector_size)))
-    model.add(LSTM(64))
-    model.add(Dense(5, activation='sigmoid'))
-
-    # try using different optimizers and different optimizer configs
-    model.compile(loss='binary_crossentropy',
-                  optimizer='adam',
-                  metrics=['accuracy'])
-    return model
-
-
-def main():
-    w2v_model = word2vec.Word2Vec.load("med250.model.bin")
-    x_train, y_train = review_to_vec(w2v_model)
-    
-    print('Pad sequences (samples x time)')
-    x_train = sequence.pad_sequences(x_train)
-    print('x_train shape:', x_train.shape)
-    print('y_train shape:', y_train.shape)
-
-    lstm_model = build_model(x_train.shape[1], x_train.shape[2])
-    print('Train...')
-    lstm_model.fit(x_train, y_train,
-              batch_size=batch_size,
-              epochs=15)
-    score, acc = lstm_model.evaluate(x_train, y_train,batch_size=batch_size)
-    print ("score: %d" %score)
-    print ("acc: %d" %acc)
+            score = 0
+            terms = aspect_term[target_aspect]
+            for sentence in content:
+                for term in terms:
+                    if term in sentence:
+                        sentence_segment = self.segment(sentence)
+                        for character in sentence_segment:
+                            if character in neg_words:
+                                score -= 1
+                            elif character in pos_words:
+                                score += 1
+            opinion = 0
+            if score > 0:
+                opinion = 1
+            elif score < 0:
+                opinion = -1
+            file.write('%s,%d\n' %(line[0], opinion))
 
 if __name__ == '__main__':
-    main()
+    reviewOpinion = ReviewOpinion()
+    reviewOpinion.main()
